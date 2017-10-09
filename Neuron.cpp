0e89c765b275b89099909c3f22d596137b553268
ArianeJ <32455034+ArianeJ@users.noreply.github.com>
2017-10-09 14:18:03 +0200

    Create Neuron.cpp

---
+++ Neuron.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include<fstream>
+#include "Neuron.hpp"
+using namespace std;
+
+double Neuron::GetTension() const {       
+	return tension;}
+															//the pgrm doesn't work from that line
+void Neuron::prochaine_tension(double Iext, double h, int n,double Vreset,double Vth){
+	for (int i(1);i<=n;++i){						 		//to make an update of the Neuron features
+		if (tension >=Vth){                             	//if the tension is to high, tension = Vreset
+			tension = Vreset;}
+		else{
+			tension=tension*exp(-h/tau)+Iext*resistance*(1-exp(-h/tau));}; // equa diff solution
+			temps= temps + h ;};};
+			
+int Neuron::nombre_pic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset){
+	int pic(0);                                             //pike are counted and stocked with time
+	double temps_pic [2][n];
+	for (int i(1);i<=n;++i){
+		prochaine_tension(Iext,h,1, Vreset,Vth);                                    
+		temps_pic[0][i-1]=temps;
+			if (tension >=Vth){
+				pic+=1;
+				temps_pic[1][i-1]=1;}                   	//in the table, "1" means pike,  "0" means none
+			else {
+				temps_pic[1][i-1]=0;};
+			};
+	cout<<temps_pic<<endl;
+	return pic;};
