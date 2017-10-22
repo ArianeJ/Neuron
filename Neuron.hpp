b9b6bb473e15fa30f6b7720f3559f1a4aac9f246
ArianeJ <32455034+ArianeJ@users.noreply.github.com>
2017-10-22 22:26:15 +0200

    Update Neuron.hpp

--- Neuron.hpp
+++ Neuron.hpp
@@ -11,7 +11,13 @@ class Neuron {
 		double tension;
 		bool refractory;
 	public:
-	    double GetTension() const;                                                         
-		void prochaine_tension(double Iext, double h, int n,double Vreset,double Vth);
-		int nombre_pic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset);
+	    double GetTension() const;
+	    double GetTime() const;
+	    bool GetRefr() const;
+	    void SetTension(double V);                                                         
+	    void prochaine_tension(double Iext, double h, int n,double Vreset,double Vth);
+	    int nombre_pic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset);
+	    Neuron(double R, double t, double tau, double V, bool refr)
+		: resistance(R),temps(t),tau(tau),tension(V),refractory(refr)
+		{}
 };
