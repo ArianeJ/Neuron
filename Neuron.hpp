fa2aeff36cfb691e26e119e642668e9e6d00ee88
ArianeJ <32455034+ArianeJ@users.noreply.github.com>
2017-10-23 14:59:02 +0200

    Update Neuron.hpp

--- Neuron.hpp
+++ Neuron.hpp
@@ -1,6 +1,8 @@
-#include<iostream>
-#include<vector>
-#include<fstream>
+#ifndef NEURON
+#define NEURON
+#include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
 
 class Neuron {
@@ -15,9 +17,9 @@ class Neuron {
 	    double GetTime() const;
 	    bool GetRefr() const;
 	    void SetTension(double V);                                                         
-	    void prochaine_tension(double Iext, double h, int n,double Vreset,double Vth);
-	    int nombre_pic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset);
-	    Neuron(double R, double t, double tau, double V, bool refr)
-		: resistance(R),temps(t),tau(tau),tension(V),refractory(refr)
-		{}
+	    void ProchaineTension(double Iext, double h, int n,double Vreset,double Vth);
+	    int NombrePic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset);
+	    Neuron(double R, double t, double tau, double V, bool refr);
 };
+
+#endif
