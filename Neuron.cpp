912f975e8dfa8974f6df1d38f66b7fba3812462d
ArianeJ <32455034+ArianeJ@users.noreply.github.com>
2017-10-23 14:59:44 +0200

    Update Neuron.cpp

--- Neuron.cpp
+++ Neuron.cpp
@@ -1,9 +1,14 @@
-#include<iostream>
-#include<math.h>
-#include<vector>
-#include<fstream>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <fstream>
 #include "Neuron.hpp"
+
 using namespace std;
+
+Neuron::Neuron(double R, double t, double tau, double V, bool refr)
+		: resistance(R),temps(t),tau(tau),tension(V),refractory(refr)
+		{}
 
 double Neuron::GetTension() const {       
 	return tension;}
@@ -17,7 +22,7 @@ bool Neuron::GetRefr() const {
 void Neuron::SetTension(double V){
 	tension = V;}
 															
-void Neuron::prochaine_tension(double Iext, double h, int n,double Vreset,double Vth){
+void Neuron::ProchaineTension(double Iext, double h, int n,double Vreset,double Vth){
 	for (int i(1);i<=n;++i){						 		//to make an update of the Neuron features
 		if (tension >=Vth){                             	//if the tension is to high, tension = Vreset
 			tension = Vreset;
@@ -27,11 +32,11 @@ void Neuron::prochaine_tension(double Iext, double h, int n,double Vreset,double
 			temps= temps + h ;
 			refractory = false;};};
 			
-int Neuron::nombre_pic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset){
+int Neuron::NombrePic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset){
 	int pic(0);                                             //pike are counted and stocked with time
 	double temps_pic [2][n];
 	for (int i(1);i<=n;++i){
-		prochaine_tension(Iext,h,1, Vreset,Vth);                                    
+		ProchaineTension(Iext,h,1, Vreset,Vth);                                    
 		temps_pic[0][i-1]=temps;
 			if (tension >=Vth){
 				pic+=1;
@@ -40,4 +45,4 @@ int Neuron::nombre_pic(double tini,double tfin,double Vth,int n,double h,double 
 				temps_pic[1][i-1]=0;};
 			};
 	cout<<temps_pic<<endl;
-	return pic;};
+return pic;};
