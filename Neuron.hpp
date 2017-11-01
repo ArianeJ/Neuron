a491094d01943edf4b4a254cbcf0ce3d4650a525
ArianeJ <32455034+ArianeJ@users.noreply.github.com>
2017-11-01 21:48:57 +0100

    Update Neuron.hpp

--- Neuron.hpp
+++ Neuron.hpp
@@ -11,12 +11,16 @@ class Neuron {
 		double temps;
 		double tau;
 		double tension;
+		bool spike;
 		bool refractory;
+		double J;				//cet attribut va différencier neurone excitateur et neurone inhibiteur
 	public:
 	    double GetTension() const;
 	    double GetTime() const;
+	    bool GetIfSpike() const;
 	    bool GetRefr() const;
-	    void SetTension(double V);                                                         
+	    void SetTension(double V);
+	    void SetRefr(bool refr);                                                      
 	    void ProchaineTension(double Iext, double h, int n,double Vreset,double Vth,double CE,double J);
 	    int NombrePic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset,double CE,double J);
 	    Neuron(double R, double t, double tau, double V, bool refr);
