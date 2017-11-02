c3d9f408727ff940d6cec4a4b82271206539da1e
ArianeJ <32455034+ArianeJ@users.noreply.github.com>
2017-11-02 21:52:58 +0100

    Update Neuron.hpp
    Deletion of the nombre_pic function, put in a more useful way in the main

--- Neuron.hpp
+++ Neuron.hpp
@@ -22,7 +22,6 @@ class Neuron {
 	    void SetTension(double V);
 	    void SetRefr(bool refr);                                                      
 	    void ProchaineTension(double Iext, double h, int n,double Vreset,double Vth,double CE,double J);
-	    int NombrePic(double tini,double tfin,double Vth,int n,double h,double Iext,double Vreset,double CE,double J);
 	    Neuron(double R, double t, double tau, double V, bool refr);
 };
 
