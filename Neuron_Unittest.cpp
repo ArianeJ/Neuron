83a8d078562b5350699a41fa18a7c4657c43fd54
ArianeJ <32455034+ArianeJ@users.noreply.github.com>
2017-11-09 20:49:39 +0100

    Update Neuron_Unittest.cpp

--- Neuron_Unittest.cpp
+++ Neuron_Unittest.cpp
@@ -4,80 +4,61 @@
 #include <vector>
 #include <cstdlib>
 #include <fstream>
-int main(){							// informations given in the paper
-	int const d(15);						// D = d * 0.1e-3 
-	double const Vth(2.0e-2);
-	double const Vreset(0.0);
-	double const H(0.1e-3);
-	int const h(1);						// H = h * 0.1e-3
-	int const CE(4);					//excitatory connection
-	int const CI(1);					//inhibitory connection
-	double const JI(0.5e-3);				
-	double const JE(0.1e-3);				
-	int const tref(20);					//Tref = 2 ms
+						
+int const d(15);						
+double const Vth(2.0e-2);
+double const Vreset(0.0);
+double const H(0.1e-3);
+int const h(1);						
+int const CE(4);					
+int const CI(1);					
+double const JI(0.5e-3);				
+double const JE(0.1e-3);				
+int const tref(20);					
 	
-	int const nombreIntervalle(1000);		
-	double const Iext(0);
-	int const Nbr_Neuron(10);			
+int const nombreIntervalle(10);		
+double const Iext(0);
+int const Nbr_Neuron(10);			
 	
 										
 	
-	vector<Neuron>neurons;	// creation of neurons
-	neurons.reserve(Nbr_Neuron);
-	for(int i(0);i<8;++i){
-		neurons.push_back(Neuron(JE));};	//excitator neurons
-	for(int i(8);i<10;++i){
-		neurons.push_back(Neuron(JI));};	//inhibitor neurons
+vector<Neuron>neurons;	
+neurons.reserve(Nbr_Neuron);
+for(int i(0);i<8;++i){
+	neurons.push_back(Neuron(JE));};	
+for(int i(8);i<10;++i){
+	neurons.push_back(Neuron(JI));};	
 		
-	TEST (Neuron construction, Jtest){
-		EXPECT_EQ(neurons[7].GetJ(),JE);
-		EXPECT_EQ(neurons[7].GetJ(),JI);
-		}
+TEST (Neuron construction, Jtest){	///are the exctitator and the inhibitor neurons in the good place in the neuron ?
+	EXPECT_EQ(neurons[7].GetJ(),JE);
+	EXPECT_EQ(neurons[7].GetJ(),JI);
+	}
 	
-	int globalClock(0);    					//real time = globalClock * 0.1e-3
+vector<vector<int> >connection(Nbr_Neuron, vector<int>(CE+CI));		
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int>UniformDistribE(0,7);
+uniform_int_distribution<int>UniformDistribJ(8,9);
+for(int i(0);i<Nbr_Neuron;++i){
+	for(int j(0);j<5;++j){
+		connection[i][j]=UniformDistribE(gen);	
+	};										
+	connection[i][5]=UniformDistribJ(gen);
+};
 	
-	vector<vector<int> >connection(Nbr_Neuron, vector<int>(CE+CI));		//	initialization of connections
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<int>UniformDistribE(0,7);
-	uniform_int_distribution<int>UniformDistribJ(8,9);
-	for(int i(0);i<Nbr_Neuron;++i){
-		for(int j(0);j<5;++j){
-			connection[i][j]=UniformDistribE(gen);	//the table contain the n° of the neuron which make the n°j connection with neuron n°i
-		};										
-		connection[i][5]=UniformDistribJ(gen);
-	};
+TEST (Distribution, Connection){	///are the chosen neurons for connection in the good range ?
+	EXPECT_LT(connection[0][4],8);
+	EXPECT_GT(connection[0][5],7);
+	}
 	
-	TEST (Distribution, Connection){
-		EXPECT_LT(connection[0][4],8);
-		EXPECT_GT(connection[0][5],7);
-		}
-	
-	cout<<"connection initialise"<<endl;
-	
-	vector<vector<double> >buffer(Nbr_Neuron,vector<double>(nombreIntervalle + d + 1)); //la case tmax + d peut aussi recevoir une valeur lin 61
-	vector<vector<bool> >refract(Nbr_Neuron,vector<bool>(nombreIntervalle +1));		//we guess it is initialized with false
-	                          
-	ofstream file_spikes("spikes");
+TEST (ProchaineTension, Expect refract){	///does the update function what it is supposed to to a spiking neuron ?
+	Neuron N(JE)
+	N.SetTension(Vth);
+	N.ProchaineTension(0.0,H,1,Vreset,Vth);
+	EXPECT_EQ(N.GetTension(),Vreset);
+	EXPECT_EQ(N.GetRefr(),true);}
 		
-	for(int k(1);k<nombreIntervalle;++k){				//action
-		for(int i(0);i<12000;++i){
-			neurons[i].SetRefr(refract[i][globalClock]);
-			neurons[i].SetTension(neurons[i].GetTension() + buffer[i][globalClock]);
-			if (neurons[i].ProchaineTension(Iext,H,1,Vreset,Vth) == true){
-				for(int elem : connection[i]){
-					buffer[elem][globalClock + d]+= neurons[i].GetJ();
-					cout<<"le buffer se remplit"<<endl;	//segmentation fault
-				};
-				for(int j(globalClock);j<globalClock + tref;++j){
-					refract[i][j]=true;
-					cout<<"le tableau refract se remplit"<<endl;
-				};
-				file_spikes<<globalClock<<"\t"<<i<<"\n";
-				cout<<globalClock<<"\t"<<i<<"\n";
-			};
-		};
-		globalClock += h;
-	};
-	file_spikes.close();
-return 0 ;}
+int main() {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
