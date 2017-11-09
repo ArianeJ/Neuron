a7b59c7341d0c21b24a46bde92433c615ff6866c
ArianeJ <32455034+ArianeJ@users.noreply.github.com>
2017-11-09 20:43:50 +0100

    Update Neuron.cpp

--- Neuron.cpp
+++ Neuron.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-Neuron::Neuron(double J)		
+Neuron::Neuron(double J)	///the only difference betwenn neurons are their inhibitory or excitatory nature, determined by J
 		: resistance(2.0e10),temps(0.0),tau(0.02),tension(0.0),spike(false),refractory(false),J(J),buffer(16,0)
 		{}
 
@@ -36,12 +36,12 @@ void Neuron::SetRefr(bool refr){
 	refractory = refr;}
 															
 bool Neuron::ProchaineTension(double Iext, double h, int n,double Vreset,double Vth){
-	static random_device rd;
+	static random_device rd;		
 	static mt19937 gen(rd());
-	static poisson_distribution<>PoissonDistrib(2);
+	static poisson_distribution<>PoissonDistrib(2);	///excitations coming from the rest of the brain
 	for (int i(1);i<=n;++i){
-		if (refractory == true){
-			tension = Vreset;
+		if (refractory == true){		///the neuron cannot spike if it is refractory
+			tension = Vreset;			
 			spike = false;}
 		else if (tension >=Vth){
 			tension = Vreset;
@@ -52,5 +52,5 @@ bool Neuron::ProchaineTension(double Iext, double h, int n,double Vreset,double 
 			};
 			temps= temps + h ;
 	}
-	return spike;
+	return spike;						/// this assessment is useful in the main
 }
